e4836f76af64d949ec5480e0efd404ec6d194f27
lrptm <lrptm@users.noreply.github.com>
2026-07-31 12:15:35 -0400

    Remove 'euros' - too many false positives

--- fb_watch.py
+++ fb_watch.py
@@ -34,7 +34,7 @@ def fetch_catalog():
 KEYWORDS = [
     "football", "soccer",
     "fifa", "ea fc", "efootball",
-    "world cup", "euros", "copa america", "nations league",
+    "world cup", "copa america", "nations league",
     "que miras bobo", "argentina", "spain",
     "messi", "ronaldo", "mbappe", "haaland", "neymar",
 ]
